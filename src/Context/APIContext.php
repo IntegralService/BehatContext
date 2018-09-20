df8189f141cb1f5ede77796761d9df0ccfc82572
julien <julien@integral-service.fr>
2018-09-20 15:07:58 +0200

    Add email functionalities

--- src/Context/APIContext.php
+++ src/Context/APIContext.php
@@ -6,6 +6,7 @@ use Behat\Symfony2Extension\Context\KernelAwareContext;
 use Behat\Symfony2Extension\Context\KernelDictionary;
 use Behatch\Context\RestContext;
 use Symfony\Component\Security\Core\User\UserInterface;
+use PHPUnit\Framework\Assert as Assertions;
 
 /**
  * APIContext
@@ -112,6 +113,111 @@ class APIContext extends RestContext implements KernelAwareContext
         $now->add(new \DateInterval('PT'.$tokenTtl.'S'));
 
         return $now->format('U');
+    }
+
+    /**
+     * @Then /^I should get an email on "(?P<email>[^"]+)" with:$/
+     */
+    public function iShouldGetAnEmail($email, \Behat\Gherkin\Node\PyStringNode $text)
+    {
+        $error     = sprintf('No message sent to "%s"', $email);
+        $profile   = $this->getSymfonyProfile();
+        $collector = $profile->getCollector('swiftmailer');
+
+        foreach ($collector->getMessages() as $message) {
+
+            // Checking the recipient email and the X-Swift-To
+            // header to handle the RedirectingPlugin.
+            // If the recipient is not the expected one, check
+            // the next mail.
+            $correctRecipient = array_key_exists(
+                $email, $message->getTo()
+            );
+
+            $headers = $message->getHeaders();
+            $correctXToHeader = false;
+            if ($headers->has('X-Swift-To')) {
+                $correctXToHeader = array_key_exists($email,
+                    $headers->get('X-Swift-To')->getFieldBodyModel()
+                );
+            }
+
+            if (!$correctRecipient && !$correctXToHeader) {
+                continue;
+            }
+
+            try {
+                // checking the content
+                $assertion = Assertions::assertContains(
+                    $text->getRaw(), $message->getBody()
+                );
+
+                // If we're here, the assertion was OK
+                $this->lastMatchedEmail = $message;
+
+                return $assertion;
+            } catch (AssertException $e) {
+                $error = sprintf(
+                    'An email has been found for "%s" but without '.
+                    'the text "%s".', $email, $text->getRaw()
+                );
+            }
+        }
+
+        throw new \Behat\Mink\Exception\ExpectationException($error, $this->getSession());
+    }
+
+    /**
+     * Assumes an email has been identified by a previous step,
+     * e.g. through 'I should get an email on "test@test.com" with:'.
+     *
+     * @When /^I click on the "([^"]*)" link in the email$/
+     */
+    public function iGoToInTheEmail($linkSelector)
+    {
+        if (!$this->lastMatchedEmail) {
+            throw new LogicException('No matched email found from previous step');
+        }
+
+        $match = $this->lastMatchedEmail;
+        $crawler = new \Symfony\Component\DomCrawler\Crawler($match->getBody());
+
+        $linkEl = $crawler->selectLink($linkSelector);
+        Assertions::assertNotNull($linkEl);
+
+        $link = $linkEl->attr('href');
+        Assertions::assertNotNull($link);
+
+        $this->getSession()->visit($link);
+    }
+
+    /**
+     * @return type
+     * @throws UnsupportedDriverActionException
+     * @throws RuntimeException
+     */
+    private function getSymfonyProfile()
+    {
+        /** @var DriverInterface */
+        $driver = $this->getSession()->getDriver();
+        if (!$driver instanceof \Behat\Symfony2Extension\Driver\KernelDriver) {
+            throw new \Behat\Mink\Exception\UnsupportedDriverActionException(
+                'You need to tag the scenario with '.
+                '"@mink:symfony2". Using the profiler is not '.
+                'supported by %s', $driver
+            );
+        }
+
+        $profile = $driver->getClient()->getProfile();
+        if (false === $profile) {
+            throw new \RuntimeException(
+                'The profiler is disabled. Activate it by setting '.
+                'framework.profiler.only_exceptions to false and '.
+                'framework.profiler.collect to true in your config'
+            );
+        }
+
+        return $profile;
     }
 
 }
